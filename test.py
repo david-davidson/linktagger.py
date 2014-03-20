d57e9020c7a390df278d38d36cbbc40db29df4c4
David Davidson <david.davidson@logos.com>
2014-03-20 08:39:33 -0700

    Updated that's handling basic tagging pretty darn well

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 import fileinput
 import re
-glt = "TESTGLT"
+glt = "?TESTGLT"
 for line in fileinput.input(inplace=1, backup='.backup'):
-    line = re.sub('<a([^>]*)href="([^"\#]*)(\#[^"]*)(\?[^"]*)"','<a\\1href="\\2\\4\\3"' + glt, line.rstrip())
-    line = re.sub('<a([^>]*)href="([^"]*http[^"?\#]*)(\#[^"]*)"','<a\\1href="\\2' + glt + '\\3"' + glt, line.rstrip()) # <= Super messed up
-    # line = re.sub('', '<a\\1href="\\2\\3', line.rstrip())
-    # s/<a(.*?)href="([^"]*http[^#?"]*?)(#[^\"]*?)*"/<a$1href="$2\?$glt$3"/g;
+    line = re.sub('<a([^>]*)href="([^"\#]*)(\#[^"]*)(\?[^"]*)"','<a\\1href="\\2\\4\\3"', line.rstrip())
+    line = re.sub('<a([^>]*)href="([^"]*http[^#?"]*?)#([^\"]*?)"','<a\\1href="\\2' + glt + '#\\3"', line.rstrip()) # <= Works, but ONLY on links w/ section ID
     print(line)
