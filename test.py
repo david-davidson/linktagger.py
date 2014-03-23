39ca488322537c2b3f985865c6f89f723da49bac
David Davidson <david.davidson@logos.com>
2014-03-23 13:37:21 -0700

    Version with the basic GLT-building functionality in place!

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+# ^ Note "python3", not "python", because Cygwin installs both. You may not need to do this--run "python -V"; if your version is 3+, you can just set the location to /python
 # Ideal command-line syntax? "glt *.txt -backup -rf"
 import fileinput
 import re
@@ -21,12 +22,8 @@ def getparameters(mode):
 	if mode == "-backup":
 		backupmode = ".backup"
 
-# Expand wildcards
-def expand(word):
-	for file in glob.glob(word):
-		process(file)
-
 # Parse command-line input
+# We no longer need this
 def process(word):
 	global filestotag
 	word = word.replace("\"\"", "")
@@ -34,7 +31,8 @@ def process(word):
 		getparameters(word)
 		word = None
 	if word != None and word.find("*") != -1:
-		expand(word)
+		for file in glob.glob(word):
+			process(file) # Expand wildcards, and shoot individual files right back into process()
 		word = None
 	if word != None and word == __file__: # Remove the script name itself
 		word = None
@@ -44,27 +42,46 @@ def process(word):
 
 # Begin script
 for word in sys.argv:
-	process(word)
+	word = word.replace("\"\"", "")
+	if word != __file__: # Don't include the script's own name
+		filestotag.append(word)
+for file in filestotag:
+	if file[0] == "-": # Remove, interpret parameters preceded by a hyphen
+		filestotag.remove(file)
+		getparameters(file)
+for file in filestotag:
+	if file.find("*") != -1: # Expand wilcards
+		filestotag.remove(file)
+		for expandedfile in glob.glob(file):
+			filestotag.append(expandedfile) # Shoot expanded files into the list
 for file in filestotag:
 	if os.path.isfile(file) is not True:
 		print("LOL WUT \""+ file + "\" is not a file")
 		filestotag.remove(file)
 	if file.endswith(".backup"):
 		filestotag.remove(file)
-length = len(filestotag)
 if len(filestotag) is 0:
 	print("No files to tag!")
 else:
+	person = input('Enter your name: ')
+	print('Hello', person)
+	source = input('Enter source: ')
+	print(source)
+	medium = input('Enter medium: ')
+	content = input('Enter content: ')
+	campaign = input('Enter campaign: ')
+	glt = 'utm_source=' + source + '&utm_medium=' + medium + '&utm_content=' + content + '&utm_campaign=' + campaign
+	print(glt)
 	for file in filestotag:
 		print(file)
-	for line in fileinput.input(filestotag, inplace=1, backup=backupmode): # Only problem: if you do *.html, it treats the second HTML file as sys.argv[2]--that is, as the mode, not a file!
+	for line in fileinput.input(filestotag, inplace=1, backup=backupmode):
 	    line = re.sub('<a([^>]*)href="([^"\#]*)(\#[^"]*)(\?[^"]*)"','<a\\1href="\\2\\4\\3"', line.rstrip()) # Put section IDs at the end
-	    line = re.sub('<a([^>]*)href="([^"]*http[^#?"]*?)"','<a\\1href="\\2?' + glt + '"', line.rstrip()) # <= Tag links without any section ID
-	    line = re.sub('<a([^>]*)href="([^"]*http[^#?"]*?)#([^\"]*?)"','<a\\1href="\\2?' + glt + '#\\3"', line.rstrip()) # <= Tag links with section ID, before the ID
-	    line = re.sub('<a([^>]*)href="([^"]*http[^#?"]*?)(\?(?![^#"]*?utm_)[^#"]*?)"','<a\\1href="\\2\\3&' + glt + '"', line.rstrip()) # <= Tag links with other parameters (but no GLT) and no section ID
-	    line = re.sub('<a([^>]*)href="([^"]*http[^#?"]*?)(\?(?!.*?utm_).*?)(#*[^#"]*)"','<a\\1href="\\2\\3&' + glt + '\\4"', line.rstrip()) # <= Tag links with other parameters (but no GLT) and a section ID at the end
+	    line = re.sub('<a([^>]*)href="([^"]*http[^#?"]*?)"','<a\\1href="\\2?' + glt + '"', line.rstrip()) # Tag links without any section ID
+	    line = re.sub('<a([^>]*)href="([^"]*http[^#?"]*?)#([^\"]*?)"','<a\\1href="\\2?' + glt + '#\\3"', line.rstrip()) # Tag links with section ID, before the ID
+	    line = re.sub('<a([^>]*)href="([^"]*http[^#?"]*?)(\?(?![^#"]*?utm_)[^#"]*?)"','<a\\1href="\\2\\3&' + glt + '"', line.rstrip()) # Tag links with other parameters (but no GLT) and no section ID
+	    line = re.sub('<a([^>]*)href="([^"]*http[^#?"]*?)(\?(?!.*?utm_).*?)(#*[^#"]*)"','<a\\1href="\\2\\3&' + glt + '\\4"', line.rstrip()) # Tag links with other parameters (but no GLT) and a section ID at the end
 	    line = re.sub('<a([^>]*)href="([^"]*http[^"]*?[^"]*)">','<a\\1href="\\2" target="_blank">', line.rstrip()) # Append target="_blank"
-	    print(line)
+	    print(line) # Writes directly to the file
 	print("Done!")
 #print(os.path.basename(__file__))
 #person = input('Enter your name: ')
