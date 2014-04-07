32d53ccefdc1055e244e2763e8924c35b72708fe
David Davidson <david.davidson@logos.com>
2014-04-06 21:13:53 -0700

    Simplified the checks for unreadable/unwritable formats

--- linktagger.py
+++ linktagger.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
 # ^ Update this with your computer's path
 
-import fileinput, re, sys, os, os.path, glob, fnmatch, codecs
+import fileinput, re, sys, os, os.path, glob, fnmatch
 
-#Set initial variables
+# Set initial variables
 filestotag = []
+taggedfiles = []
 backupmode = "" # none
 recursion = False
 removeglt = False
 tagging = False
-path = "."
+# path = "."
+# Compile the regular expressions
+idbeforeend = re.compile(r'<a([^>]*)href="([^"\#]*)(\#[^"]*)(\?[^"]*)"')
+hasglt = re.compile(r'(\?|\&|\&amp;)utm_[^?#"]*')
+no_id = re.compile(r'<a([^>]*)href="([^"]*http[^#?"]*?)"')
+hasid = re.compile(r'<a([^>]*)href="([^"]*http[^#?"]*?)#([^\"]*?)"')
+parametersno_id = re.compile(r'<a([^>]*)href="([^"]*http[^#?"]*?)(\?(?![^#"]*?utm_)[^#"]*?)"')
+parametershasid = re.compile(r'<a([^>]*)href="([^"]*http[^#?"]*?)(\?(?!.*?utm_).*?)(#*[^#"]*)"')
+notargetblank = re.compile(r'<a([^>]*)href="([^"]*http[^"]*?[^"]*)">')
+
+# Begin functions
 
 # Interpret parameters
 def interpretparameter(mode):
@@ -23,6 +34,7 @@ def interpretparameter(mode):
 	if mode == "-strip":
 		removeglt = True
 
+# Remove parameters
 def checkparameters(filestotag):
 	newfiles = []
 	for file in filestotag:
@@ -32,14 +44,16 @@ def checkparameters(filestotag):
 			newfiles.append(file) # Remove them; they're not files
 	return newfiles
 
+# Expand wildcards through subdirectories
 def expandrecursively(filestotag):
 	recursivematches = []
-	global path
 	for file in filestotag:
 		if file.find(os.path.sep) != -1:
 			splitversion = file.rsplit(os.path.sep, 1)
 			file = splitversion[1]
 			path = splitversion[0]
+		else:
+			path = "."
 		for root, dirnames, filenames in os.walk(path):
 			filenames = [f for f in filenames if not f[0] == '.']
 			dirnames[:] = [d for d in dirnames if not d[0] == '.']
@@ -47,90 +61,66 @@ def expandrecursively(filestotag):
 				recursivematches.append(os.path.join(root, filename))
 	return recursivematches
 
+# Expand wildcards in current working directory
 def expandwildcards(filestotag):
 	wildcardmatches = []
 	for file in filestotag:
 		for expandedfile in glob.glob(file):
-			# sexpandedfile = "." + os.path.sep + expandedfile
 			wildcardmatches.append(expandedfile) # Shoot expanded files into the list
 	return wildcardmatches
 
-def checkencoding(filestotag):
-	newfiles = []
-	for currentfile in filestotag:
-		clean = True
-		try:
-			f = codecs.open(currentfile, encoding="windows-1252", errors="strict")
-			try:
-				for line in f:
-					pass
-				try:
-					f = codecs.open(currentfile, encoding="utf-8", errors="strict")
-					try:
-						for line in f:
-							pass
-					except:
-						clean = False
-						print("Skipping", currentfile, "because it's unreadable in utf-8")
-				except:
-					clean = False
-					pass
-			except:
-				clean = False
-				print("Skipping", currentfile, "because it's unreadable in windows-1252")		
-		except:
-			clean = False
-			pass
-		if clean == True:
-			newfiles.append(currentfile)
-	return newfiles
-
-def sanitize(filestotag):
+def removeextras(filestotag):
 	newfiles = []
 	for file in filestotag:
-		if os.access(file, os.W_OK) is True:
-			if os.access(file, os.R_OK) is True:
-				if file.endswith(".backup") is False:
-					if os.path.abspath(file) != os.path.abspath(__file__):
-						newfiles.append(file)
-			else:
-				print("Skipping", file, ": can't read from it")
-		else:
-			print("Skipping", file, ": can't write to it")
+		if file.endswith(".backup") is False:
+			if os.path.abspath(file) != os.path.abspath(__file__):
+				newfiles.append(file)
+	return newfiles
+
+def checkencoding(filestotag):
+	newfiles = []
+	for file in filestotag:
+			with open(file, "r+") as f:
+				try:
+					for line in f:
+						pass
+					newfiles.append(file)
+				except:
+					print("Skipping", file, " because we can't read and write to it")
+					continue
 	return newfiles
 
 def printfinalfiles(filestotag):
 	newfiles = []
 	for file in filestotag:
 		try:
-			print(file) # Somehow certain docs can sneak through the UTF-8 and windows-1252 filters and still throw an exception in print(). We remove them, too.
+			print(file) # Docs that include weird characters in the name--but not the body--can still trigger an exception; remove them, too.
 			newfiles.append(file)
 		except:
 			pass
 	return newfiles
 
-# BEGIN USER INTERACTION
+# Begin user interaction:
 for word in sys.argv:
 	word = word.replace("\"\"", "") # Remove quotation marks, which we only needed to prevent the shell from expanding wildcards
-	if word != __file__:
-		filestotag.append(word) # Create initial list of arguments
+	filestotag.append(word)
 filestotag = checkparameters(filestotag) # Remove and interpret arguments preceded by "-"
 if recursion == True:
 	filestotag = expandrecursively(filestotag)
 else:
 	filestotag = expandwildcards(filestotag)
-filestotag = sanitize(filestotag)
-filestotag = checkencoding(filestotag)
+filestotag = checkencoding(filestotag) # Keep only the files we can read and write to
+filestotag = removeextras(filestotag) # Remove .backups and the script itself
 if len(filestotag) is 0:
 	print("\nNo files to tag!")
 else:
 	print("\nFiles to tag:")
-	filestotag = printfinalfiles(filestotag) # Perform final check and prints all files to tag
+	filestotag = printfinalfiles(filestotag) # Perform final check and print all files to tag
 	type=input("\nType e to paste in existing GLT, or n to build new GLT: ")
 	if type == "e" or type == "E":
 		tagging = True
 		glt = input("Right-click and paste in GLT: ")
-		if glt[0] == "?" or glt[0] == "&": # If there's a leading question mark, trim it
+		if glt[0] == "?" or glt[0] == "&": # If there's a leading question mark or ampersand, trim it
 			glt = glt[1:]
 	elif type == "n" or type == "N":
 		tagging = True
@@ -142,22 +132,57 @@ else:
 		glt = glt.lower()
 	addtargetblank = input("Add target=\"_blank\"? y/n: ")
 	for file in filestotag:
+		changes = False
 		print("Scanning", file, "...")
 		for line in fileinput.input(file, inplace=1, backup=backupmode):
-		    line = re.sub('<a([^>]*)href="([^"\#]*)(\#[^"]*)(\?[^"]*)"','<a\\1href="\\2\\4\\3"', line.rstrip()) # Put section IDs at the end
-		    if removeglt == True:
-		    	line = re.sub('(\?|\&|\&amp;)utm_[^?#"]*','', line.rstrip()) # Strip GLT
-		    if tagging == True:
-			    line = re.sub('<a([^>]*)href="([^"]*http[^#?"]*?)"','<a\\1href="\\2?' + glt + '"', line.rstrip()) # Tag links without any section ID
-			    line = re.sub('<a([^>]*)href="([^"]*http[^#?"]*?)#([^\"]*?)"','<a\\1href="\\2?' + glt + '#\\3"', line.rstrip()) # Tag links with section ID, before the ID
-			    line = re.sub('<a([^>]*)href="([^"]*http[^#?"]*?)(\?(?![^#"]*?utm_)[^#"]*?)"','<a\\1href="\\2\\3&' + glt + '"', line.rstrip()) # Tag links with other parameters (but no GLT) and no section ID
-			    line = re.sub('<a([^>]*)href="([^"]*http[^#?"]*?)(\?(?!.*?utm_).*?)(#*[^#"]*)"','<a\\1href="\\2\\3&' + glt + '\\4"', line.rstrip()) # Tag links with other parameters (but no GLT) and a section ID at the end
-		    if addtargetblank == "y" or addtargetblank == "Y":
-			    line = re.sub('<a([^>]*)href="([^"]*http[^"]*?[^"]*)">','<a\\1href="\\2" target="_blank">', line.rstrip()) # Append target="_blank"
-		    print(line) # Prints to file
-	filecount = len(filestotag)
-	if filecount == 1:
-		filereference = "file"
+			try:
+			    m = re.search(idbeforeend, line)
+			    if m:
+			    	changes = True
+			    line = re.sub(idbeforeend,'<a\\1href="\\2\\4\\3"', line.rstrip()) # Put section IDs at the end
+			    if removeglt == True:
+			    	m = re.search(hasglt, line)
+			    	if m:
+			    		changes = True
+				    	line = re.sub(hasglt,'', line.rstrip()) # Strip GLT
+			    if tagging == True:
+			    	m = re.search(no_id, line)
+			    	if m:
+			    		changes = True
+			    		line = re.sub(no_id,'<a\\1href="\\2?' + glt + '"', line.rstrip()) # Tag links without any section ID
+		    		m = re.search(hasid, line)
+		    		if m:
+		    			changes = True
+		    			line = re.sub(hasid,'<a\\1href="\\2?' + glt + '#\\3"', line.rstrip()) # Tag links with section ID, before the ID
+	    			m = re.search(parametersno_id, line)
+	    			if m:
+	    				changes = True
+	    				line = re.sub(parametersno_id,'<a\\1href="\\2\\3&' + glt + '"', line.rstrip()) # Tag links with other parameters (but no GLT) and no section ID
+	    			m = re.search(parametershasid, line)
+	    			if m:
+	    				changes = True
+	    				line = re.sub(parametershasid,'<a\\1href="\\2\\3&' + glt + '\\4"', line.rstrip())
+			    if addtargetblank == "y" or addtargetblank == "Y":
+			    	m = re.search(notargetblank, line)
+			    	if m:
+			    		changes = True
+			    		line = re.sub(notargetblank,'<a\\1href="\\2" target="_blank">', line.rstrip()) # Append target="_blank"
+			    print(line) # Prints to file
+			except:
+				print("aha!")
+				continue
+		if changes == True:
+			taggedfiles.append(file)
+	scannedcount = len(filestotag)
+	taggedcount = len(taggedfiles)
+	if scannedcount == 1:
+		scannedref = "file"
 	else:
-		filereference = "files"
-	print("Done!", filecount, filereference, "scanned")
+		scannedref = "files"
+	if taggedcount == 1:
+		taggedref = "file"
+	else:
+		taggedref = "files"
+	for file in taggedfiles:
+		print("Tagged:", file)
+	print("Done!", scannedcount, scannedref, "scanned;", taggedcount, taggedref, "changed.")
